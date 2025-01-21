5187bc1942088a57f35efce43fedf540175cd2a1
josh-meier <joshmeier456@gmail.com>
2025-01-21 15:04:02 -0600

    added fixed pages back

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,19 +1,8 @@
 import Image from "next/image";
 import { Geist, Geist_Mono } from "next/font/google";
 import { useState } from "react";
-import Link from "next/link";
 // import '../styles/navbar.css'
 
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 // export default function Home() {
 //   return (
@@ -160,9 +149,9 @@ const Navbar2 = () => {
             </div>
             <div className="hidden md:block">
               <div className="ml-4 flex items-center space-x-4">
-                <Link href="/experience" className="text-black hover:bg-gray-200 rounded-lg p-2 text-xl">Experience</Link>
-                <Link href="/projects" className="text-black hover:bg-gray-200 rounded-lg p-2 text-xl">Projects</Link>
-                <Link href="/contact" className="text-black  hover:bg-gray-200 rounded-lg p-2 text-xl">Contact</Link>
+                <a href="/experience" className="text-black hover:bg-gray-200 rounded-lg p-2 text-xl">Experience</a>
+                <a href="/projects" className="text-black hover:bg-gray-200 rounded-lg p-2 text-xl">Projects</a>
+                <a href="/contact" className="text-black  hover:bg-gray-200 rounded-lg p-2 text-xl">Contact</a>
               </div>
             </div>
             <div className="md:hidden flex items-center">
@@ -183,9 +172,9 @@ const Navbar2 = () => {
         {isNavBarVisible && (
           <div className="md:hidden">
             <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
-              <Link href="/experience" className="text-black block hover:bg-gray-200 rounded-lg p-2">Experience</Link>
-              <Link href="/projects" className="text-black block hover:bg-gray-200 rounded-lg p-2">Projects</Link>
-              <Link href="/contact" className="text-black block hover:bg-gray-200 rounded-lg p-2">Contact</Link>
+              <a href="/experience" className="text-black block hover:bg-gray-200 rounded-lg p-2">Experience</a>
+              <a href="/projects" className="text-black block hover:bg-gray-200 rounded-lg p-2">Projects</a>
+              <a href="/contact" className="text-black block hover:bg-gray-200 rounded-lg p-2">Contact</a>
             </div>
           </div>
         )}
@@ -267,39 +256,9 @@ export default function Home(){
         <span className="font-medium">Beware!</span> This website is currently still under construction.
       </div>
       
-      <div className="grid grid-cols-1 md:grid-cols-2">
-        <div className="px-2 md:pt-36">
-          <h1 className="text-4xl font-semibold dark:text-white">
-            Hey, I'm Josh! 
-          </h1>
-          <p className="py-4 text-2xl text-gray-500 dark:text-gray-400">
-            I'm a software engineer graduating from Carleton College in June 2025. I most recently completed an externship with Lytup, 
-            a sustainable energy startup, and at school I've been working as a mobile app developer for Project Friendship the last 2 years.  
-            <strong className="font-normal"> I'm currently looking for full-time developer roles starting in Summer 2025</strong>!
-          </p>
-
-          <div className="collapse collapse-arrow bg-base-200">
-            <input type="checkbox" name="my-accordion-2" />
-            <div className="collapse-title text-xl font-medium">Click to learn more about me!</div>
-            <div className="collapse-content">
-              <p className="text-gray-500 dark:text-gray-400">
-                My name is Josh Meier and I am a senior Computer Science major here at Carleton! Throughout my time here, I have 
-                pursued interests in data science, cognitive science, linguistics, psychology, and architecture (I was even able to study abroad in Italy, Spain, and England!). 
-              </p>
-              <p className="text-gray-500 dark:text-gray-400 pt-4">
-                Outside of class, I am a captain of the Carleton Men's <abbr title="Division 3" className="no-underline">DIII</abbr> Track and Cross Country teams as well
-                as an avid performer and singer in the Carleton Singing Knights A Cappella group. When I'm not running or arranging music, you can also find me playing soccer, 
-                thrifting, drawing, or exploring new places and scenery.
-              </p>
-            </div>
-          </div>
-        </div>
-        <div className="px-2 pt-4 md:pt-18 ">
-          <Image className="h-auto max-w-full" width={1000} height={1000} src="/headshot.jpg" alt="Headshot of Josh Meier standing in front of Stonehenge">
-          </Image>
-        </div>
+      
         
-      </div>
+      
         
       
     </div>
